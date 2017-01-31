57a545bec23530c48cc2f11c667d4548f232abb5
Salvador Guzman <guzmansalv@gmail.com>
2017-01-31 04:35:11 -0800

    return sorted, deduped list of bad domains

--- src/main/scala/io/initialcommit/sguzman/upwork/bad/domain/Bad.scala
+++ src/main/scala/io/initialcommit/sguzman/upwork/bad/domain/Bad.scala
@@ -1,9 +1,15 @@
 package io.initialcommit.sguzman.upwork.bad.domain
 
+import com.mashape.unirest.http.Unirest
+
 import scala.io.Source
 
 object Bad {
   def main(args: Array[String]): Unit = {
-    println(Source.fromFile("/Users/salvadorguzman/Software/BadDomainsList/src/main/resources/bad-domains.txt"))
+    val domains = "/Users/salvadorguzman/Software/BadDomainsList/src/main/resources/bad-domains.txt"
+    (for(line <- Source.fromFile(domains).getLines)  yield line)
+      .map(Unirest.get(_).asString.getBody)
+      .filter(!_.startsWith("#")).toArray.sortWith(_ < _).distinct
+      .foreach(println)
   }
 }
